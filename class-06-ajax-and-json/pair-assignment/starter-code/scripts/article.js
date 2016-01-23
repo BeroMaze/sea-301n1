081582f4115af63c20585cb18d1bd53b2889f530
BeroMaze <Berning.corey@gmail.com>
2016-01-23 11:59:30 -0800

    add JSON and AJAX

--- class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -7,11 +7,6 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-// DONE: Instead of a global `articles = []` array, let's track this list of all articles directly on the
-// constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves
-// objects, which means we can add properties/values to them at any time. In this case, we have
-// a key/value pair to track, that relates to ALL of the Article objects, so it does not belong on
-// the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
 Article.prototype.toHtml = function() {
@@ -24,13 +19,6 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-// DONE: There are some other functions that also relate to articles across the board, rather than
-// just single instances. Object-oriented programming would call these "class-level" functions,
-// that are relevant to the entire "class" of objects that are Articles.
-
-// DONE: This function will take the rawData, how ever it is provided,
-// and use it to instantiate all the articles. This code is moved from elsewhere, and
-// encapsulated in a simply-named function for clarity.
 Article.loadAll = function(rawData) {
   rawData.sort(function(a,b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
@@ -38,25 +26,27 @@ Article.loadAll = function(rawData) {
 
   rawData.forEach(function(ele) {
     Article.all.push(new Article(ele));
-  })
-}
+  });
+};
 
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
 Article.fetchAll = function() {
   if (localStorage.rawData) {
-    // When rawData is already in localStorage,
-    // we can load it with the .loadAll function above,
-    // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(//TODO: What do we pass in here to the .loadAll function?
-    );
-    articleView.someFunctionToCall; //TODO: What method do we call to render the index page?
-  } else {
-    // TODO: When we don't already have the rawData,
-    // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-    // cache it in localStorage so we can skip the server call next time,
-    // then load all the data into Article.all with the .loadAll function above,
-    // and then render the index page.
-
+    var xxxx = JSON.parse(localStorage.getItem('rawData'));
+    var yyyy = JSON.parse(localStorage.getItem('rawaDataArticles'));
+    Article.loadAll(xxxx);
+    Article.loadAll(yyyy);
+    Article.all.forEach(function (item){
+      $('#articles').append(item.toHtml());
+    });
   }
-}
+  else {
+      $.getJSON('./data/hackerIpsum.json', function(data){
+        $hackerIpsum = data;
+        console.log($hackerIpsum);
+        localStorage.setItem('rawData', JSON.stringify(data));
+      });
+      $.getJSON('./data/ipsumArticles.json', function(data){
+        localStorage.setItem('rawaDataArticles', JSON.stringify(data));
+      });
+  }
+};
