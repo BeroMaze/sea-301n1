e504382ecb5f685ffdb298ac527ce7cb2b30cd9b
BeroMaze <Berning.corey@gmail.com>
2016-03-02 19:50:40 -0800

    changes

--- class-13-production-deployment/pair-assignment/starter-code/scripts/repo.js
+++ class-13-production-deployment/pair-assignment/starter-code/scripts/repo.js
@@ -5,16 +5,23 @@
 
   // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
+    $.get('https://api.github.com/users/beromaze/repos' +
+          '?per_page=100' +
+          '&sort=updated'
+    ).done(function(data) {
+      repos.all = data;
     }).done(callback);
+
+  //   $.ajax({
+  //     url: 'https://api.github.com/users/beromaze/repos' +
+  //           '?per_page=100' +
+  //           '&sort=updated',
+  //     type: 'GET',
+  //     headers: { 'Authorization': 'token ' + githubToken },
+  //     success: function(data, message, xhr) {
+  //       repos.all = data;
+  //     }
+  //   }).done(callback);
   };
 
   repos.with = function(attr) {
